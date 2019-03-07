300e3c04538b4909e676e0e21fda07ffffaae084
nyorain <nyorain@gmail.com>
2019-03-07 18:58:16 +0100

    Continue sen

---
+++ src/shaders/include/ray.glsl
@@ -0,0 +1,109 @@
+struct Ray {
+	vec3 origin;
+	vec3 dir;
+};
+
+// box defined by a transform
+// transform * box = unit cube
+struct Box {
+	mat4 transform;
+};
+
+Box box() {
+	return Box(mat4(1));
+}
+
+Box box(vec3 center) {
+	Box b = box();
+	b.transform[3].xyz = center;
+	return b;
+}
+
+Box box(vec3 center, vec3 x, vec3 y, vec3 z) {
+	return Box(mat4(vec4(x, 0), vec4(y, 0), vec4(z, 0), vec4(center, 1)));
+}
+
+vec3 multDir(mat4 transform, vec3 dir) {
+	return mat3(transform) * dir;
+}
+
+vec3 multPos(mat4 transform, vec3 pos) {
+	vec4 v = transform * vec4(pos, 1.0);
+	return vec3(v) / v.w;
+}
+
+/// Intersects the ray with the sphere at pos sphere.xyz with radius sphere.w
+float intersect(Ray r, vec4 sphere) {
+    vec3 oc = r.origin - sphere.xyz;
+    float b = dot(oc, r.dir);
+    float c = dot(oc, oc) - sphere.w * sphere.w;
+    float h = b*b - c;
+    if(h < 0.0) { // no intersection
+		return -1.0;
+	}
+	return -b - sqrt(h);
+}
+
+vec3 sphereNormal(vec3 ce, vec3 pos) {
+	return normalize(pos - ce);
+}
+
+/// Returns intersection ray parameter and box normal of ray and box.
+/// -1.0 when there is no intersection, a negative value if the
+/// ray origin is inside the box.
+float intersect(Ray r, Box b, out vec3 normal) {
+	vec3 dir = multDir(b.transform, r.dir);
+	vec3 o = multPos(b.transform, r.origin);
+
+#ifdef EXACT
+	// XXX:
+	// could this be harmful in any case?
+	// need it otherwise d,ad,t1,t2 can become NaN which might
+	// propagate through end result (only in some cases though...)
+	if(dir.x == 0.f) { dir.x += 0.00001; }
+	if(dir.y == 0.f) { dir.y += 0.00001; }
+	if(dir.z == 0.f) { dir.z += 0.00001; }
+#endif
+
+	// how many t we need to reach xi = 0 from origin
+	// might result in +/- infinity in an dimension
+	vec3 d = -o / dir;
+
+	// t needed for ray to progress 1 in dimension i
+	// might result in +/- infinity in an dimension
+	vec3 ad = abs(1.f / dir);
+
+	// ts needed for first/second xi = 1 intersections (following ray in its dir)
+	// oi + di * t1i = sign(di)
+	// oi + di * t2i = -sign(di)
+	// again: might be infinity. Well defined though:
+	vec3 t1 = d - ad;
+	vec3 t2 = d + ad;
+
+	// the t value on which the ray has entered (i.e. crossed the first
+	// intersection point; must be from the outside) the unit cube in
+	// all dimensions.
+	// That's why we need max here: the last one.
+	float tn = max(max(t1.x, t1.y), t1.z);
+
+	// the t value on which the ray has left (i.e. crossed the second
+	// intersection point; must be from the inside) the unit cube
+	// in the first dimensions.
+	// That's why we need min here: the first one.
+	float tf = min(min(t2.x, t2.y), t2.z);
+
+	// if the ray leaves the unit cube in one dimension while not
+	// having entered in all, it will never intersect.
+	// When otherwise tf < 0.0, then the first intersection, then
+	// both intersections are on negative t values.
+	if(tn > tf || tf < 0.0) {
+		return -1.f;
+	}
+
+    normal = -sign(r.dir)* step(t1.yzx, t1.xyz) * step(t1.zxy, t1.xyz);
+
+	// If the origin of the ray is *in* the box, then this
+	// returns a negative value: the first intersection of the ray
+	// behind the origin.
+	return tn;
+}
--- src/shaders/sen.frag
+++ src/shaders/sen.frag
@@ -1,5 +1,9 @@
 #version 450
 
+#extension GL_GOOGLE_include_directive : enable
+
+#include "ray.glsl"
+
 layout(location = 0) in vec2 in_uv;
 layout(location = 0) out vec4 out_color;
 
@@ -13,34 +17,82 @@ layout(set = 0, binding = 0) uniform UBO {
 
 const vec3 up = vec3(0, 1, 0);
 const vec3 lightPos = vec3(0, 5, 0);
+const int maxBounce = 2;
 
-float sphereIntersection(vec3 ro, vec3 rd, vec3 ce, float ra) {
-    vec3 oc = ro - ce;
-    float b = dot(oc, rd);
-    float c = dot(oc, oc) - ra * ra;
-    float h = b*b - c;
-    if(h < 0.0) { // no intersection
-		return -1.0;
+vec4 spheres[] = {
+	{0, 0, -3, 1},
+	{3, 0, -4, 0.5},
+	{1, 2, 3, 2},
+};
+
+Box boxes[] = {
+	box(vec3(-4.0, 0.0, 0.0)),
+	box(vec3(4.0, -4.0, 1.0)),
+};
+
+// TODO: fuck, recursion
+vec3 trace(Ray ray, int bounce);
+
+vec3 shade(vec3 pos, vec3 normal, vec3 view, int bounce) {
+	// sum of those should be 1
+	float ambientFac = 0.1;
+	float diffFac = 0.4;
+	float specFac = 0.4;
+	float reflFac = 0.1;
+
+	float shininess = 32;
+
+	vec3 col = vec3(0.1); // ambient
+	vec3 l = normalize(lightPos - pos);
+
+	// diffuse
+	col += diffFac * clamp(dot(normal, l), 0, 1);
+
+	// specular: blinn phong
+	vec3 h = normalize(-view + l);
+	col += specFac * vec3(clamp(pow(dot(h, normal), shininess), 0, 1));
+
+	// XXX: trace perfect reflextion
+	if(bounce < maxBounce) {
+		vec3 r = reflect(normal, view);
+		col += reflFac * trace(Ray(pos, r), bounce + 1);
 	}
-	return -b - sqrt(h);
+
+	return clamp(col, 0.0, 1.0);
 }
 
-vec3 sphereNormal(vec3 ce, vec3 pos) {
-	return normalize(pos - ce);
-}
+vec3 trace(Ray ray, int bounce) {
+	float t;
+	float mint = 1.f / 0.f; // infinity
+	vec3 mincol = vec3(0, 0, 0); // default, background
 
-vec3 trace(vec3 ro, vec3 rd) {
-	// scene
-	vec3 sphereCenter = vec3(0, 0, -3.f);
-	float t = sphereIntersection(ro, rd, sphereCenter, 0.5f);
-	out_color = vec4(0, 0, 0, 1);
-	if(t > 0) {
-		vec3 pos = ro + t * rd;
-		vec3 normal = sphereNormal(sphereCenter, pos);
-		return vec3(0.8 * clamp(dot(normal, normalize(lightPos - pos)), 0, 1));
-	} else {
-		return vec3(0);
+	// spheres
+	for(int i = 0; i < spheres.length(); ++i) {
+		t = intersect(ray, spheres[i]);
+		if(t > 0.0 && t < mint) {
+			mint = t;
+
+			// shade
+			vec3 pos = ray.origin + t * ray.dir;
+			vec3 normal = sphereNormal(spheres[i].xyz, pos);
+			mincol = shade(pos, normal, ray.dir, bounce);
+		}
 	}
+
+	// boxes
+	for(int i = 0; i < boxes.length(); ++i) {
+		vec3 normal;
+		t = intersect(ray, boxes[i], normal);
+		if(t > 0.0 && t < mint) {
+			mint = t;
+
+			// shade
+			vec3 pos = ray.origin + t * ray.dir;
+			mincol = shade(pos, normal, ray.dir, bounce);
+		}
+	}
+
+	return mincol;
 }
 
 void main() {
@@ -66,10 +118,11 @@ void main() {
 			vec3 ro = ubo.pos.xyz;
 			vec3 rd = normalize(az + muv.x * ax + muv.y * ay);
 
-			vec3 col = trace(ro, rd);
+			vec3 col = trace(Ray(ro, rd), 0);
 			rgb += (1 / count) * col;
 		}
 	}
 
+	// TODO: tone mapping
 	out_color = vec4(rgb, 1);
 }
