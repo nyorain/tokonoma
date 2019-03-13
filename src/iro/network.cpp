d71f59a069d4c99c2ad585c160266b5b1d191bac
nyorain <nyorain@gmail.com>
2019-03-13 22:52:58 +0100

    Dummy iro broadcast impl

--- src/iro/network.cpp
+++ src/iro/network.cpp
@@ -1,6 +1,8 @@
 #include "network.hpp"
 #include <dlg/dlg.hpp>
 #include <stage/bits.hpp>
+#include <mutex>
+#include <condition_variable>
 
 // TODO:
 // - invalid packages (currently using doi::read, only asserts size)
@@ -8,11 +10,11 @@
 // - acknowledge and stuff
 
 Socket::Socket() {
+	/*
 	auto ep = udp::endpoint(asio::ip::address_v4({127, 0, 0, 1}), port);
 	try {
 		socket_ = {ioService_, ep};
 		ep = udp::endpoint(asio::ip::address_v4({127, 0, 0, 1}), port + 1);
-		socket().connect(ep);
 
 		// wait for other side
 		socket().wait(asio::ip::udp::socket::wait_read);
@@ -33,10 +35,128 @@ Socket::Socket() {
 		socket().send(asio::buffer(&i, 4));
 		dlg_info("connection established as 1");
 	}
+	*/
+
+	auto bep = udp::endpoint(udp::v4(), broadcastPort);
+	broadcast_ = udp::socket(ioService_);
+	broadcast_->open(bep.protocol());
+	broadcast_->set_option(asio::socket_base::reuse_address(true));
+	broadcast_->set_option(asio::socket_base::broadcast(true));
+	broadcast_->bind(bep);
+
+	// port testing
+	for(auto iport = 21345; iport < 65000; ++iport) {
+		try {
+			auto ep = udp::endpoint(asio::ip::address_v4({127, 0, 0, 1}), iport);
+			socket_ = asio::ip::udp::socket(ioService_, ep);
+			break;
+		} catch(const std::system_error& exception) {
+			continue;
+		}
+	}
+
+	if(!socket_) {
+		throw std::runtime_error("Couldn't open socket");
+	}
+
+	dlg_info("socket on {}", socket().local_endpoint());
+	socket().set_option(asio::socket_base::broadcast(true));
+
+	// send broadcast
+	auto ep = udp::endpoint(asio::ip::address_v4::broadcast(), broadcastPort);
+	auto i = std::uint32_t(42);
+	socket().send_to(asio::buffer(&i, 4), ep);
+	dlg_info("sent broadcast");
+
+	// wait
+	std::uint32_t state = 0;
+
+	udp::endpoint recvep1;
+	std::uint32_t recvbuf1;
+	auto socketHandler = [&](auto& ec, auto count) {
+		if(ec || count < 4) {
+			return; // XXX: restart recv?
+		}
+		this->recvSocket(recvep1, recvbuf1, state);
+	};
+	socket().async_receive_from(asio::buffer(&recvbuf1, 4), recvep1, socketHandler);
+
+	// wait for another process's broadcast
+	udp::endpoint recvep2;
+	std::uint32_t recvbuf2;
+	auto brdHandler = [&](auto& ec, auto count) {
+		if(ec || count < 4) {
+			return; // XXX: restart recv?
+		}
+		this->recvBroadcast(recvep2, recvbuf2, state);
+	};
+	broadcast_->async_receive_from(asio::buffer(&recvbuf2, 4), recvep2, brdHandler);
+
+	// will run until connected
+	ioService_.run();
 
 	// Not sure really why this is needed though to make socket().available()
 	// work...
 	socket().non_blocking(true);
+}
+
+void Socket::recvSocket(udp::endpoint& ep, std::uint32_t& num, unsigned& state) {
+	if(state == 0) {
+		// someone received our broadcast and answers now
+		if(num == 43) {
+			// answer confirmation
+			socket().connect(ep);
+			num = 44;
+			socket().send(asio::buffer(&num, 4));
+			state = 2;
+			ioService_.stop();
+			dlg_info("received broadcast answer from {}", ep);
+			return;
+		}
+	} else if(state == 1) {
+		// we received final confirmation
+		if(num == 44) {
+			socket().connect(ep);
+			state = 2;
+			ioService_.stop();
+			dlg_info("received confirmation from {}", ep);
+			return;
+		}
+	}
+
+	dlg_warn("invalid message: {}, {}", state, num);
+
+	// continue
+	auto socketHandler = [&](auto& ec, auto count) {
+		if(ec || count < 4) {
+			return; // XXX: restart recv?
+		}
+		this->recvSocket(ep, num, state);
+	};
+	socket().async_receive_from(asio::buffer(&num, 4), ep, socketHandler);
+}
+
+void Socket::recvBroadcast(udp::endpoint& ep, std::uint32_t& num, unsigned& state) {
+	auto brdHandler = [&](auto& ec, auto count) {
+		if(ec || count < 4) {
+			return; // XXX: restart recv?
+		}
+		this->recvBroadcast(ep, num, state);
+	};
+
+	if(ep == socket().local_endpoint()) {
+		dlg_debug("filtering out own broadcast");
+		broadcast_->async_receive_from(asio::buffer(&num, 4), ep, brdHandler);
+		return;
+	}
+
+	if(num == 42) {
+		dlg_info("received broadcast from {}", ep);
+		// answer confirmation 1
+		num = 43;
+		socket().send_to(asio::buffer(&num, 4), ep);
+		state = 1;
+	}
 }
 
 void Socket::update(MsgHandler handler) {
