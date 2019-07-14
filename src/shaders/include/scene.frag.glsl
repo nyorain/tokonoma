9f2e0b8d0bd48f74d674e1ed22f83adf7402eabe
nyorain <nyorain@gmail.com>
2019-07-14 18:19:07 +0200

    WIP (broken): Alternative normal mapping

--- src/shaders/include/scene.frag.glsl
+++ src/shaders/include/scene.frag.glsl
@@ -1,33 +1,26 @@
+// http://www.thetenthplanet.de/archives/1180
 // dFdx/dFdy only defined in fragment shader
-// compute world space normal
-vec3 tbnNormal(vec3 normal, vec3 pos, vec2 uv, sampler2D normalTex) {
-	// http://www.thetenthplanet.de/archives/1180
+
+// normal: geometry normal (interpolated normal from vertices)
+// tnormal: local tangen-space normal, read from texture (but in [-1, 1] range)
+vec3 tbnNormal(vec3 normal, vec3 pos, vec2 uv, vec3 tnormal) {
 	vec3 q1 = dFdx(pos);
 	vec3 q2 = dFdy(pos);
 	vec2 st1 = dFdx(uv);
 	vec2 st2 = dFdy(uv);
 
-	vec3 t = normalize(q1 * st2.t - q2 * st1.t);
-	vec3 b = -normalize(cross(normal, t));
+	vec3 p1 = cross(q2, normal);
+	vec3 p2 = cross(normal, q1);
+	vec3 t = p2 * st1.x + p1 * st2.x;
+	vec3 b = p2 * st1.y + p1 * st2.y;
 
-	// texture contains normal in tangent space
-	// we could also just use a signed format here i guess
-	vec3 tn = texture(normalTex, uv).xyz * 2.0 - 1.0;
-	return normalize(mat3(t, b, normal) * tn);
+	float scale = inversesqrt(max(dot(t, t), dot(b, b)));
+	return normalize(mat3(scale * t, scale * b, normal) * tnormal);
 }
 
-vec3 tbnNormal(vec3 normal, vec3 pos, vec2 uv, vec3 texNormal) {
-	// http://www.thetenthplanet.de/archives/1180
-	vec3 q1 = dFdx(pos);
-	vec3 q2 = dFdy(pos);
-	vec2 st1 = dFdx(uv);
-	vec2 st2 = dFdy(uv);
-
-	vec3 t = normalize(q1 * st2.t - q2 * st1.t);
-	vec3 b = -normalize(cross(normal, t));
-
+// compute world space normal
+vec3 tbnNormal(vec3 normal, vec3 pos, vec2 uv, sampler2D normalTex) {
 	// texture contains normal in tangent space
 	// we could also just use a signed format here i guess
-	vec3 tn = texNormal * 2.0 - 1.0;
-	return normalize(mat3(t, b, normal) * tn);
+	return tbnNormal(normal, pos, uv, texture(normalTex, uv).xyz * 2.0 - 1.0);
 }
